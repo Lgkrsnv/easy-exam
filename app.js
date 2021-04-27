7de6a0d9ff31e6854dd1731037ad0f80fbe374e1
Olga <olga.dibts@ya.ru>
2021-04-27 18:25:55 +0300

    before merge

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const MongoStore = require('connect-mongo');
 const indexRouter = require('./routes/index');
 
 const app = express();
-// dbConnect();
+dbConnect();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const options = {
-  store: MongoStore.create({mongoUrl: 'mongodb://localhost:27017/studentProject'}),
+  store: MongoStore.create({ mongoUrl: 'mongodb://localhost:27017/studentProject' }),
   key: "user_sid",
   secret: "panda",
   resave: false,
@@ -38,12 +38,12 @@ app.use('/', indexRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+ 
   res.render('index');
 });
-
+router.post('/', async function(req, res, next) {
+ const { file } = req.body;
+ console.log(file);
+  res.json('file', { file });
+});
 module.exports = router;
