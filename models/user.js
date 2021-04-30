9cdf102644adfce22c2bd2545b8944bd03b9d3d6
Olga <olga.dibts@ya.ru>
2021-04-30 09:45:35 +0300

    верстка main page

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
-  phone: { type: Number},
+  phone: { type: Number },
   password: { type: String, required: true },
   orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
   role: String,
